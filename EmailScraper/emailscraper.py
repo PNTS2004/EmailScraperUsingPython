00f964e031d7b0aa63602f9f70caa0f665d082ee
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-23 21:59:40 +0530

    Imported Librabries

--- EmailScraper/emailscraper.py
+++ EmailScraper/emailscraper.py
@@ -1,1 +1,8 @@
-print('hi')
+from bs4 import BeautifulSoup
+import requests
+import requests.exceptions
+import urllib.parse
+from collections import deque
+import re
+
+user_url=str(input("Enter Target Url to Scan: "))
