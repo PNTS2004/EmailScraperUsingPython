39d6de0564d02464310cffd6e5d7481138ef4cda
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-24 23:35:34 +0530

    Continued

--- EmailScraper/emailscraper.py
+++ EmailScraper/emailscraper.py
@@ -6,3 +6,16 @@ from collections import deque
 import re
 
 user_url=str(input("Enter Target Url to Scan: "))
+urls=deque([user_url])
+
+scraped_urls=set()
+emails=set()
+
+count=0
+try:
+    while len(urls):
+        count +=1
+        if count==100:
+            break
+        url=urls.popleft()
+        scraped_urls.add(url)
