b3d5b0f519a92041c36c4fda5adf311fd90c8adc
Pooja Nokhwal <poojanokhwal04@gmail.com>
2025-05-26 23:17:41 +0530

    continued...

--- EmailScraper/emailscraper.py
+++ EmailScraper/emailscraper.py
@@ -19,3 +19,10 @@ try:
             break
         url=urls.popleft()
         scraped_urls.add(url)
+
+        parts=urllib.parse.urlsplit(url)
+        base_url='{0.scheme}://{0.netloc}'.format(parts)
+
+        path=url[:url.rfind('/')+1] if '/' in parts.path else url
+
+        print('[%d] Processing %s' %(count,url))
